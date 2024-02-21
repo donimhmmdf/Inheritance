8b3d0fc247e2e96b66f457e0ce113e81f2623e3e
donimhmmdf <donirespect07@gmail.com>
2024-02-21 11:59:25 +0700

    first commint

---
+++ MainApp.java
@@ -0,0 +1,37 @@
+
+public class MainApp {
+    public static void main(String[] args) {
+        // Person person1 = new Person();
+        // person1.name = "Hendra";
+        // person1.address = "Garut";
+
+        // Teacher teacher1 = new Teacher();
+        // teacher1.name = "Budi";
+        // teacher1.address = "Bandung";
+        // teacher1.subject = "Matematika";
+
+        Docter docter1 = new Docter();
+        docter1.name = "Elis";
+        docter1.address = "Jakarta";
+        docter1.specialist = "Dentis";
+
+        Docter docter2 = new Docter("Sandi", "Bandung", "Cardiologist");
+
+        docter1.greeting();
+        System.out.println();
+        docter2.greeting();
+
+        // Programmer programmer1 = new Programmer();
+        // programmer1.name = "Rizki";
+        // programmer1.address = "Surabaya";
+        // programmer1.technology = "Javascript";
+
+        // person1.greeting();
+        // System.out.println();
+        // teacher1.greeting();
+        // System.out.println();
+        // docter1.greeting();
+        // System.out.println();
+        // programmer1.greeting();
+    }
+}
---
+++ Programmer.java
@@ -0,0 +1,22 @@
+public class Programmer extends Person{
+    String technology;
+
+    public Programmer(String name, String address, String technology){
+        super(name, address);
+        this.technology = technology;
+    }
+    public Programmer(){
+        
+    }
+
+    void hacking(){
+        System.out.println("I can hacking a website.");
+    }
+    void coding(){
+        System.out.println("I can create a application using technology : " + technology + ".");
+    }
+    void greeting(){
+        super.greeting();
+        System.out.println("My job is a " + technology + " programmer.");
+    }
+}
